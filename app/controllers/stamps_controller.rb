f1bec11c24b8af286e1535ec00fbfd7358644ddc
hanacat299 <flower.catlnver63@gmail.com>
2024-07-15 18:16:41 +0900

    コントローラーの作成

--- app/controllers/stamps_controller.rb
+++ app/controllers/stamps_controller.rb
@@ -1,7 +1,21 @@
 class StampsController < ApplicationController
   def index
   end
+  def new
+    @stamp = Stamp.new
+  end
 
+  def create
+    @stamp = Stamp.new(stamp_params)
+  end
+
+  private
+
+  def stamp_params
+    params.require(:stamp).permit(
+      :image, :number, :name, :height, :width, :desgin_id, :stamp_color_id, :stamp_number, :paper_color_id, :paper_name, :paper_weight, :temperature, :push_time, :push_pressure, :set_time_id, :remark
+    )
+  end
   
 
 end
